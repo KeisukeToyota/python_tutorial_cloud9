29d5421f3b459af2552eb7f78cddc6935767971d
peperoncino000 <hm.pudding0715@gmail.com>
2017-02-09 18:46:53 +0900

    commit

--- init.sh
+++ init.sh
@@ -1,16 +1,20 @@
-sudo rm -f /usr/bin/python
-sudo ln -s /usr/bin/python3 /usr/bin/python
+cd ~/workspace/
 
-# pip install numpy # already satisfied
-# pip install scipy # already satisfied
-# pip install matplotlib # already satisfied
-pip install pandas --user
-pip install seaborn --user
-pip install jupyter --user
-pip install scikit-learn --user
-pip install ipython -U --user
+git clone https://github.com/yyuu/pyenv.git ~/workspace/.pyenv
 
-echo 'export PATH=/home/ubuntu/.local/bin:$PATH' >> ~/.bashrc
+export PYENV_ROOT="$HOME/workspace/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+pyenv install anaconda3-4.1.1
+pyenv rehash
+pyenv global anaconda3-4.1.1
+
+sudo pip install seaborn
+
+echo 'export PYENV_ROOT="$HOME/workspace/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 jupyter notebook --generate-config
 echo 'c.NotebookApp.ip = "*"' >> ~/.jupyter/jupyter_notebook_config.py
@@ -18,4 +22,4 @@ echo 'c.NotebookApp.open_browser = False' >> ~/.jupyter/jupyter_notebook_config.
 echo 'c.NotebookApp.notebook_dir = "/home/ubuntu/workspace"' >> ~/.jupyter/jupyter_notebook_config.py
 echo 'c.NotebookApp.port = 8080' >> ~/.jupyter/jupyter_notebook_config.py
 
-jupyter notebook
+source ~/.bashrc
